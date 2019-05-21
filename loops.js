8cd9c9f470e0f11a26d96d6e0d5dd39a60e79a62
Anil Kishan <kishan.anil@gmail.com>
2019-05-21 22:26:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,9 +9,8 @@ function forLoop(array){
 return array
 }
 
-function whileLoop(number) {
-  while (number > 0) {
-    {console.log(--number)}
-    return "done"
-  }
+function whileLoop(n){
+  while(n>0)
+  {console.log(n--)}
+  return "done"
 }
